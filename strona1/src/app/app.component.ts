2e08d856ce82769ae94a0a4508357ee0dd032d01
Czubek892 <cjakub2345@gmail.com>
2024-04-25 20:48:39 +0200

    loading added

--- strona1/src/app/app.component.ts
+++ strona1/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './component/loading/loading.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, AppComponent, LoadingComponent]
 })
 export class AppComponent {
   title = 'strona1';
